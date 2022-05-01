0b4dc5ed2c77d2d076831235d9ce75a40553d991
murnesty <murnesty@gmail.com>
2022-05-01 19:04:59 +0800

    fix 5

--- js/lunch.js
+++ js/lunch.js
@@ -254,11 +254,11 @@ Murnesty.Lunch = function() {
     }
 
     function init() {
-        $.getJSON('@Url.Content("~/data/restaurant_list.json")', function(shops) {
+        $.getJSON("data/restaurant_list.json", function(shops) {
             _setupRestaurants(shops);
         });
 
-        $.getJSON('@Url.Content("~/data/user_list.json")', function(users) {
+        $.getJSON("data/user_list.json", function(users) {
             _setupOrders(users);
         });
     }
